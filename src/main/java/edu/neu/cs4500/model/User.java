dfc8183a4398af19f8b97744b6780f70eca806f9
Isabel Tripp <stagestruck1@gmail.com>
2019-01-23 22:15:00 -0500

    Added constructor to User class in model

--- src/main/java/edu/neu/cs4500/model/User.java
+++ src/main/java/edu/neu/cs4500/model/User.java
@@ -8,13 +8,46 @@ public class User {
 	private String password;
 	private String firstName;
 	private String lastName;
-	// provide a public set and get method for each of the class variables above
-	// make sure to use proper naming conventions for Java methods, e.g.,
-	// methods should be camelcase. Here's an example using the firstName variable:
+	
+	//Constructor when all fields are given 
+	public User(int id, String username, String password, String firstName, String lastName) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
  	public String getFirstName() {
 		return firstName;
 	}
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	
+ 	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 }
